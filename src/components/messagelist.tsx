197ab8bf3d72d66654295e06059d605cbc4a25ed
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-07-05 08:29:20 +0100

    💄 added navbar

--- src/components/messagelist.tsx
+++ src/components/messagelist.tsx
@@ -1,13 +1,12 @@
 
-import john from "../assets/john.jpg";
 import { chatlist } from "../constants";
 
 const Messagelist = () => {
     return (<>
         {
             chatlist.map((chat) => (
-                <div className="flex flex-row  p-3  hover:bg-gray-200 hover:bg-opacity-5 rounded-2xl">
-                    <img src={john} className="rounded-full w-12 h-12" />
+                <div key={chat.key} className="flex flex-row  p-3  hover:bg-gray-200 hover:bg-opacity-5 rounded-2xl">
+                    <img src={chat.image} className="rounded-full w-12 h-12" />
                     <div className='flex flex-col flex-1 px-4 '>
                         <div className='text-lg font-bold hidden text-white md:block'>{chat.username}</div>
                         <div className='text-md font-medium text-primary hidden md:block'>{chat.message}</div>
@@ -17,11 +16,8 @@ const Messagelist = () => {
                         <div className="flex justify-end py-1">
                         <span className="bg-slate-600 rounded-xl px-2 text-white ">
                                 {chat.notification}
-
                             </span>
                         </div>
-                           
-
                     </div>
                 </div>
             ))
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,10 +1,35 @@
+import john from "../assets/john.jpg";
+import evelyn from "../assets/Evelyn.jpg";
+import sarah from "../assets/sarah.jpg";
+import james from "../assets/logo.jpg";
+import {BsBookmark} from "react-icons/bs";
+import {BsPerson} from "react-icons/bs";
+import {FiSettings} from "react-icons/fi";
+import {GiNightSleep} from "react-icons/gi";
+import {MdAnimation} from "react-icons/md";
+import {AiOutlineQuestionCircle} from "react-icons/ai";
+import {BiBug} from 'react-icons/bi';
+import SwitchButton from "../components/switch_button";
+
 export const chatlist =
 [
-    {key:1,username:"John doe",message:"Hey , there",lastmessage:"15:14",notification:0},
-    {key:2,username:"Sarah",message:"Morning",lastmessage:"Jun 14",notification:44},
-    {key:3,username:"Ela",message:"Night",lastmessage:"Feb 23",notification:2},
-    {key:4,username:"James",message:"Going out ?",lastmessage:"Jul 2,2022",notification:398},
-    {key:5,username:"Kevin",message:"See ya",lastmessage:"Apr 16,2022",notification:1},
-    {key:6,username:"Alaysa",message:"Good luck",lastmessage:"Feb 5,2022",notification:33},
-    {key:7,username:"Barry Allen",message:"wait what ?",lastmessage:"Jan 1,2020",notification:33},
+    {key:1,image:john,username:"John doe",message:"Hey , there",lastmessage:"15:14",notification:0},
+    {key:2,image:sarah,username:"Sarah",message:"Morning",lastmessage:"Jun 14",notification:44},
+    {key:3,image:evelyn,username:"evelyn",message:"Night",lastmessage:"Feb 23",notification:2},
+    {key:4,image:james,username:"James",message:"Going out ?",lastmessage:"Jul 2,2022",notification:398},
+    // {key:5,image:"",username:"Kevin",message:"See ya",lastmessage:"Apr 16,2022",notification:1},
+    // {key:6,image:"",username:"Alaysa",message:"Good luck",lastmessage:"Feb 5,2022",notification:33},
+    // {key:7,image:"",username:"Barry Allen",message:"wait what ?",lastmessage:"Jan 1,2020",notification:33},
+]
+
+export const navbar=[
+    {key:1,icon:<BsBookmark/>,text:"Saved Messages",},
+    {key:2,icon:<BsPerson/>,text:"Contacts",},
+    {key:3,icon:<FiSettings/>,text:"Settings"},
+    {key:4,icon:<GiNightSleep/>,text:"Night Mode",toggleButton:<SwitchButton/>},
+    {key:5,icon:<MdAnimation/>,text:"Animations",toggleButton:<SwitchButton/>},
+    {key:6,icon:<AiOutlineQuestionCircle/>,text:"Telegram Features"},
+    {key:7,icon:<BiBug/>,text:"Report Bug"},
+    // {key:8,icon:"K",text:"Switch to K Version",toggleButton:<SwitchButton/>},
+
 ]
